368be4aafe098198fe97778bc259ef4b1a849021
suhabe <suhabe@bugrara.com>
2025-06-27 12:20:52 -0500

    add coverage script

---
+++ simplemultisig/coverage.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -Eeuxoa pipefail
+
+
+rm -rf lcov.info coverage
+forge coverage --report lcov --ir-minimum
+genhtml --ignore-errors inconsistent lcov.info --branch-coverage --output-dir coverage
+ open coverage/index.html
